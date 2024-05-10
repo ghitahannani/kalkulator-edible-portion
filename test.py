c53abb7f72dab2ae6aa6009fa2433084beaf3c8e
ghitahannani <165451955+ghitahannani@users.noreply.github.com>
2024-05-10 11:41:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -6,12 +6,12 @@ st. title('Hai! Kami siap membantu anda! :chicken: :wave:')
 st. write('Aplikasi ini merupakan hasil project dari kami, kelompok 9 untuk memenuhi tugas akhir mata kuliah logika pemrograman komputer. Silahkan gulir layar anda kebawah!')
 
 st.subheader('Anggota Kelompok 9', divider='rainbow')
-st.write ('
+st.write ('''
           1. Bunga Sekar Ningrum (2320514)
           2. Delvina Neva Ghita Hannani (2320516)
           3. Dilla Aulia Efendi (2320518)
           4. Gea Veronika Caniago (2320525)
-          5. Muhammad Syaukani Akbar (2320538)')
+          5. Muhammad Syaukani Akbar (2320538)''')
 
 st. markdown(':fish::broccoli::potato::onion::sunflower::apple::fish::broccoli::potato::onion::sunflower::apple::fish:')
 
@@ -19,13 +19,13 @@ st.subheader('Apa Itu Edible Portion', divider='rainbow')
 st.write ('Edible portion adalah bagian dari makanan yang dapat dimakan atau dikonsumsi setelah bagian-bagian yang tidak dapat dimakan atau tidak diinginkan telah dihilangkan.')
 
 st.subheader('Manfaat dan Fungsi Mengetahui Edible Portion', divider='rainbow')
-st.write ('
+st.write ('''
           1. Pengelolaan Bahan Baku: Dengan mengetahui berapa persentase dari bahan baku yang sebenarnya dapat dimakan, produsen makanan dapat mengelola persediaan mereka dengan lebih efisien.
           2. Efisiensi Pengolahan: Mengetahui bagian yang dapat dimakan dari bahan mentah memungkinkan produsen makanan untuk mengoptimalkan proses pengolahan dan mengurangi pemborosan.
           3. Perencanaan Menu yang Lebih Sehat: Restoran atau katering yang memahami edible portion dari berbagai bahan makanan dapat merencanakan menu yang lebih sehat dan seimbang nutrisinya.
           4. Pengurangan Pemborosan Makanan: Dengan memahami berapa banyak dari bahan mentah yang sebenarnya dapat dimakan, individu dan bisnis dapat mengurangi pemborosan makanan dengan memanfaatkan sebanyak mungkin dari bahan tersebut.
           5. Manajemen Biaya: Dengan mengetahui berapa banyak dari bahan yang dapat dimakan, perusahaan makanan dan individu dapat mengelola biaya mereka dengan lebih baik dengan menghindari pembelian atau penggunaan bahan yang tidak diperlukan.
-          6. Peningkatan Kesadaran Nutrisi: Memahami bagian makanan yang sebenarnya dapat dimakan membantu individu untuk membuat pilihan makanan yang lebih sehat dan memperhitungkan asupan nutrisi mereka dengan lebih baik.')
+          6. Peningkatan Kesadaran Nutrisi: Memahami bagian makanan yang sebenarnya dapat dimakan membantu individu untuk membuat pilihan makanan yang lebih sehat dan memperhitungkan asupan nutrisi mereka dengan lebih baik.''')
 
 st. markdown(':fish::broccoli::potato::onion::sunflower::apple::fish::broccoli::potato::onion::sunflower::apple::fish:')
 
